7ed93fa95adaff184ff2596452cdf3f3bcd32132
Felipe Santos <felipecassiors@gmail.com>
2018-11-13 13:48:13 -0200

    Initial commit

---
+++ src/locadora/Cliente.java
@@ -0,0 +1,63 @@
+package locadora;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String sexo;
+    private int idade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " | ID: " + id;
+    }
+}
